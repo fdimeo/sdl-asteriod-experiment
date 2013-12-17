93b01106e1c6098291865aabd9f50cad62e2defe
fdimeo <frank_a_dimeo@yahoo.com>
2013-12-17 13:05:58 -0500

    working with all_asteroids class

--- SDL_asteroids.cpp
+++ SDL_asteroids.cpp
@@ -6,8 +6,10 @@ http://nothingtocode.blogspot.com
 
 #include <SDL2/SDL.h>
 #include <cstdbool>
+#include <cstdlib>
 #include <iostream>
 #include <unordered_set>
+#include <utility>
 
 /* Window resolution */
 #define WINDOW_WIDTH 640
@@ -70,17 +72,10 @@ public:
    asteroid(position pos, velocity vel, double a_vel, direction rotational_direction);
    ~asteroid();
    void update();
+   position getPosition();
    void render(SDL_Renderer *pRenderer);
 };
 
-// helper function that updates and renders all asteroids that are currently alive
-void update_all_asteroids(std::unordered_set< asteroid* > all_asteroids)
-{
-   for (auto it : all_asteroids){
-      it->update();
-      it->render(Main_Renderer);
-   }
-}
 
 double operator *(double d, direction dir)
 {
@@ -126,28 +121,87 @@ void asteroid::render(SDL_Renderer *pRenderer)
    SDL_RenderCopyEx(pRenderer, pSprite_texture, &srcrect, &dstrect, angle, NULL, SDL_FLIP_NONE);
 }
 
+position asteroid::getPosition()
+{
+   return (std::make_pair(dstrect.x, dstrect.y));
+}
 
 
+class all_asteroids {
+private:
+   std::unordered_set<asteroid *> m_all_asteroids;  // a set of all the asteroids alive in the game
+   std::pair<unsigned int, unsigned int> m_window_boundaries;  // the boundaries for the asteroids
 
-std::unordered_set<asteroid *> all_asteroids;
+public:
+   all_asteroids(std::pair<unsigned int, unsigned int> window_boundaries);
+   void spawn();
+   void update();     
+   ~all_asteroids();  // delete all asteroids created on the heap
+};
+
+all_asteroids::all_asteroids(std::pair<unsigned int, unsigned int> window_boundaries):m_window_boundaries(window_boundaries)
+{
+}
+
+void all_asteroids::spawn()
+{
+   if (m_all_asteroids.size() < MAX_NUM_ASTEROIDS)
+   {
+      srand (time(NULL));
+      
+  
+      m_all_asteroids.insert(new asteroid(std::make_pair((rand() % m_window_boundaries.first),(rand() % m_window_boundaries.second)), 
+            std::make_pair(((rand() % 9)+1)/5.0,((rand() % 9)+1)/5.0), 
+            1.2, 
+            (rand() % 1) ? direction::RIGHT : direction::LEFT));
+   }
+}
+
+// updates and renders all asteroids that are currently alive
+void all_asteroids::update()
+{
+   position pos;
+   std::unordered_set<asteroid *> tmp_set;
+   
+
+   for (auto it : m_all_asteroids){
+      it->update();
+      pos = it->getPosition();
+      if ((pos.first > m_window_boundaries.first) || (pos.second > m_window_boundaries.second)){
+         tmp_set.insert(it);
+      }
+      else{
+         it->render(Main_Renderer);
+      }
+   }
+
+   // remove any asteroids that have gone out of scope
+   for( auto it : tmp_set){
+      m_all_asteroids.erase(it);
+   }
+}
+
+all_asteroids::~all_asteroids()
+{
+   std::cout << "whacking all spawned asteroids" << std::endl;
+   // wipe out any and all asteroids created on the heap
+   m_all_asteroids.clear();
+}
 
 
 Uint32 update_game(Uint32 interval, void *param)
 {
    // run the timer until the main thread signals up to stop
    if(!SDL_SemValue(stop_game_timer)){
+
+      all_asteroids *pAsteroids = (reinterpret_cast< all_asteroids* > (param));
+
+      pAsteroids->spawn();
       
-      if (all_asteroids.size() < MAX_NUM_ASTEROIDS)
-      {
-         all_asteroids.insert(new asteroid(std::make_pair(50,300), std::make_pair(1,-1), 2.4, direction::RIGHT));
-      }
    }
 
    else{
       std::cout << "game timer stopped" << std::endl;
-
-      // we're done, destroy all the spawned asteroids
-      all_asteroids.clear();
 
       // we've been requested to stop this timer...so tell the main thread that we have
       SDL_SemPost(game_timer_stopped);
@@ -163,14 +217,17 @@ Uint32 update_screen(Uint32 interval, void *param)
    // run the timer until the main thread signals up to stop
    if(!SDL_SemValue(stop_refresh_timer)){
 
+      all_asteroids *pAsteroids = (reinterpret_cast< all_asteroids* > (param));
+
       SDL_RenderCopy(Main_Renderer, bkgd_texture, NULL, NULL);
    
-      update_all_asteroids(all_asteroids);
+      // update all the game asteroids
+      pAsteroids->update();
 
       SDL_RenderPresent(Main_Renderer);
    }
    else{
-      std::cout << "refresh timer stopped" << std::endl;
+      //      std::cout << "refresh timer stopped" << std::endl;
       // we've been requested to stop this timer...so tell the main thread that we have
       SDL_SemPost(refresh_timer_stopped);
    }
@@ -206,66 +263,69 @@ int main( int argc, char* args[] )
          WINDOW_HEIGHT,
          SDL_WINDOW_SHOWN);
 
-   std::cout << "updating screen every " << TIMER_UPDATE << " mS" << std::endl;
+      // create the game asteroids instance, and tell it where the window boundaries are
+      all_asteroids game_asteroids(std::make_pair(WINDOW_WIDTH, WINDOW_HEIGHT));
+
+      std::cout << "updating screen every " << TIMER_UPDATE << " mS" << std::endl;
    
-   Main_Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+      Main_Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
-   SDL_Surface *tmpsurf = SDL_LoadBMP( "background.bmp" );
-   bkgd_texture = SDL_CreateTextureFromSurface(Main_Renderer, tmpsurf);
-   SDL_FreeSurface(tmpsurf);
+      SDL_Surface *tmpsurf = SDL_LoadBMP( "background.bmp" );
+      bkgd_texture = SDL_CreateTextureFromSurface(Main_Renderer, tmpsurf);
+      SDL_FreeSurface(tmpsurf);
 
-   // start our frame update timer
-   refresh_timer = SDL_AddTimer(TIMER_UPDATE, update_screen, NULL);
+      // start our frame update timer
+      refresh_timer = SDL_AddTimer(TIMER_UPDATE, update_screen, &game_asteroids);
 
-   // start our game timer
-   game_timer = SDL_AddTimer(GAME_UPDATE, update_game, NULL);
+      // start our game timer and pass the asteroids instance
+      game_timer = SDL_AddTimer(GAME_UPDATE, update_game, &game_asteroids);
 
 
-   // get the audio drivers available
-   std::cout << "number of audio drivers available: " << SDL_GetNumAudioDrivers() << std::endl;
+      // get the audio drivers available
+      std::cout << "number of audio drivers available: " << SDL_GetNumAudioDrivers() << std::endl;
 
-   SDL_RendererInfo RenderInfo;
+      SDL_RendererInfo RenderInfo;
 
-   SDL_GetRendererInfo(Main_Renderer,
-      &RenderInfo);
+      SDL_GetRendererInfo(Main_Renderer,
+         &RenderInfo);
 
-   std::cout << "flags are: " << RenderInfo.flags << std::endl;
+      std::cout << "flags are: " << RenderInfo.flags << std::endl;
     
 
-   while( running )
-   {
-      while( SDL_PollEvent( &event ) != 0 )
+      while( running )
       {
-         switch(event.type){
-            case (SDL_QUIT):
-               running = false;
-            case (SDL_KEYDOWN):
-               std::cout << "key down is: " <<  event.key.keysym.scancode << std::endl;
-               if (event.key.keysym.scancode == 20) running = false;
-            default:
-               continue;
+         while( SDL_PollEvent( &event ) != 0 )
+         {
+            switch(event.type){
+               case (SDL_QUIT):
+                  running = false;
+               case (SDL_KEYDOWN):
+                  std::cout << "key down is: " <<  event.key.keysym.scancode << std::endl;
+                  if (event.key.keysym.scancode == 20) running = false;
+               default:
+                  continue;
+            }
          }
+
       }
+   }
 
-  }
- }
+   // clean up 
+   // signal the timer threads to stop running so we can get rid of it
+   SDL_SemPost(stop_refresh_timer);
+   SDL_SemPost(stop_game_timer);
 
- // clean up 
- // signal the timer threads to stop running so we can get rid of it
- SDL_SemPost(stop_refresh_timer);
- SDL_SemPost(stop_game_timer);
+   // wait until the timer threads have finished the last iteration
+   SDL_SemWait(refresh_timer_stopped);
+   SDL_SemWait(game_timer_stopped);
 
- // wait until the timer threads have finished the last iteration
- SDL_SemWait(refresh_timer_stopped);
- SDL_SemWait(game_timer_stopped);
+   // remove the timers
+   SDL_RemoveTimer(refresh_timer);
+   SDL_RemoveTimer(game_timer);
 
- // remove the timers
- SDL_RemoveTimer(refresh_timer);
- SDL_RemoveTimer(game_timer);
-
- SDL_DestroyTexture(bkgd_texture);
- SDL_DestroyRenderer(Main_Renderer);
- SDL_DestroyWindow( window );
- SDL_Quit();
- return 0;
+   SDL_DestroyTexture(bkgd_texture);
+   SDL_DestroyRenderer(Main_Renderer);
+   SDL_DestroyWindow( window );
+   SDL_Quit();
+   return 0;
 }
